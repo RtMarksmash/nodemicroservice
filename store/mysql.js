09bcb1c51500ea262ab7f37daa66d1fd3b9ca542
mario mora <jorgemariomoramolina@gmail.com>
2023-11-08 17:38:06 -0500

    function insert on db

--- store/mysql.js
+++ store/mysql.js
@@ -42,7 +42,7 @@ function handleCon() {
 handleCon();
 
 
-function list(table, id) {
+function list(table) {
     return new Promise((resolve, reject) => {
         connection.query(`SELECT * FROM ${table} `, (err, data) => {
             if (err) {
@@ -53,8 +53,59 @@ function list(table, id) {
 
 
     })
-}
+};
+
+
+function get(table, id) {
+    return new Promise((resolve, reject) => {
+        connection.query(`SELECT * FROM ${table} WHERE id=${id} `, (err, data) => {
+            if (err) {
+                return reject(err)
+            };
+            resolve(data);
+        });
+
+    });
+};
+
+function insert(table, data) {
+    return new Promise((resolve, reject) => {
+        connection.query(`INSERT INTO ${table} SET? `, data, (err, result) => {
+            if (err) {
+                return reject(err)
+            };
+            resolve(result);
+        });
+
+    });
+};
+
+
+function update(table, data) {
+    return new Promise((resolve, reject) => {
+        connection.query(`INSERT INTO ${table} SET? WHERE id=`, [data, data.id], (err, result) => {
+            if (err) {
+                return reject(err)
+            };
+            resolve(result);
+        });
+
+    });
+};
+
+
+function upsert(table, data) {
+    if (data && data.id) {
+        return update(table, data);
+    } else {
+        return insert(table, data);
+    };
+
+};
+
 
 module.exports = {
-    list
+    list,
+    get,
+    upsert
 }
